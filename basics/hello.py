1a9345bbd2182a3bed2108a6414a7177184c12d4
Abhishek Abhishek <aabhishe@redhat.com>
2025-04-05 18:06:36 +0530

    starting python course

---
+++ basics/hello.py
@@ -0,0 +1,5 @@
+def hello():
+    return print("Hello World!")
+
+if __name__ == "__main__":
+    hello()
---
+++ basics/user_input.py
@@ -0,0 +1,6 @@
+def hello():
+    name = input("Enter your name: ")
+    return print("Hello " + name + "!")
+
+if __name__ == "__main__":
+    hello()
