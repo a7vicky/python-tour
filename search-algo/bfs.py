e189848e1bb780998008e97038d53fb52e0aa841
Abhishek Abhishek <aabhishe@redhat.com>
2025-09-26 23:31:16 +0530

    bfs and dfs search

---
+++ search-algo/bfs.py
@@ -0,0 +1,27 @@
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+
+visited = [] # List to keep track of visited nodes
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node):
+  visited.append(node)
+  queue.append(node)
+
+  while queue:
+    s = queue.pop(0) 
+    print (s, end = " ") 
+
+    for neighbour in graph[s]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+# Driver Code
+bfs(visited, graph, '5')
